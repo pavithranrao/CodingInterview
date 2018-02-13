031d335d377dd56101dc6406a3919d7b0ab2ed76
Pavithran Ramachandran <pavithranrao.1512@gmail.com>
2018-02-13 00:00:39 -0500

    Binary Search

---
+++ src/BinarySearch.scala
@@ -0,0 +1,47 @@
+import scala.annotation.tailrec
+
+object BinarySearch {
+
+  def bst(input: Array[Int], target: Int): Option[Int] = {
+    @tailrec
+    def recursiveSearch(low: Int, high: Int): Option[Int] =
+      (low + high) / 2 match {
+        case _ if high < low => None
+        case mid if input(mid) > target => recursiveSearch(low, mid - 1)
+        case mid if input(mid) < target => recursiveSearch(mid + 1, high)
+        case mid => Some(mid)
+      }
+
+    //    def recursiveSearch(low: Int, high: Int): Option[Int] = {
+    //      val mid = (low + high) / 2
+    //      input(mid).compare(target) match {
+    //        case 1 => recursiveSearch(low, mid - 1)
+    //        case -1 => recursiveSearch(mid + 1, high)
+    //        case 0 => Some(mid)
+    //
+    //        case _ if high < low => None
+    //      }
+    //    }
+
+    recursiveSearch(0, input.length - 1)
+  }
+
+  def main(args: Array[String]): Unit = {
+    //    val array = Array(1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6)
+    val array = (1 to 10).toArray
+    println(s"Given array : ${array.zipWithIndex.mkString(", ")}")
+
+    val target = 6
+
+    val answer = bst(array, target)
+    if (answer.isDefined) {
+      println(s"The number $target is found at ${answer.get}")
+    }
+    else {
+      println(s"The number $target is not found")
+    }
+  }
+
+}
+
+
