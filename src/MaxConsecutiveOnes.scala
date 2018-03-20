087461a381b1c9066f69d6edef0b150b7fb0da0f
Pavithran Ramachandran <pavithranrao.1512@gmail.com>
2018-03-20 00:14:11 -0400

    Max Consequtive Ones (Kadanes' Algorithm)

---
+++ src/MaxConsecutiveOnes.scala
@@ -0,0 +1,23 @@
+object MaxConsecutiveOnes {
+
+  // Kadanes' Algorithm Variant
+  def getMaxConsecutiveOnes(nums: Array[Int]): Int =
+    nums.foldLeft((0, 0)) {
+      case ((maxUpToHere, maxSoFar), present) =>
+        if (present == 1) {
+          val newMax = maxUpToHere + 1
+          (newMax, newMax max maxSoFar)
+        } else {
+          (0, maxSoFar)
+        }
+    }._2
+
+  def main(args: Array[String]): Unit = {
+    val array = Array(1, 1, 0, 1, 1, 1)
+    println(s"The input array is : ${array.mkString(", ")}")
+
+    val answer = getMaxConsecutiveOnes(array)
+    println(s"The maximum consecutive one are : $answer")
+  }
+
+}
