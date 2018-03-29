bfb002e3ccf096052f66926f234c557026a712a4
Pavithran Ramachandran <pavithranrao.1512@gmail.com>
2018-03-29 00:10:25 -0400

    String Difference (LeetCode)

---
+++ src/StringDifference.scala
@@ -0,0 +1,19 @@
+object StringDifference {
+
+  def findTheDifference(s: String, t: String): Char = {
+    s.zip(t).foldLeft(t.last) {
+      case (acc, (a, b)) =>
+        (acc ^ a ^ b).toChar
+    }
+  }
+
+
+  def main(args: Array[String]): Unit = {
+    val s = "a"
+    val t = "aa"
+
+    val answer = findTheDifference(s, t)
+    println(s"$answer")
+  }
+
+}
