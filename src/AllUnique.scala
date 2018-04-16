4e461ec579d8d1cd486e9f0acfdb1b6c321fd5ce
Pavithran <pavithranrao.1512@gmail.com>
2018-04-16 00:15:43 -0400

    all unique elements LeetCode

--- src/AllUnique.scala
+++ src/AllUnique.scala
@@ -1,3 +1,16 @@
 object AllUnique {
 
+  def allUnique(input: Array[Int]): Boolean = {
+    input.length > 0 && input.toSet.size == input.length
+  }
+
+  def main(args: Array[String]): Unit = {
+    val input = Array(1, 2, 6, 3, 9)
+    val answer = allUnique(input)
+    println(answer)
+    
+    assert(!allUnique(Array()))
+  }
+
+
 }
