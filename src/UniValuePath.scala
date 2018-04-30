23dc335b1b343e20bd3d9a5fd8fb6c44b8029d17
Pavithran <pavithranrao.1512@gmail.com>
2018-04-30 01:23:56 -0400

    UniValue Path LeetCode

--- src/UniValuePath.scala
+++ src/UniValuePath.scala
@@ -1,3 +1,32 @@
-class UniValuePath {
+object UniValuePath {
+
+  class TreeNode(var _value: Int) {
+    var value: Int = _value
+    var left: TreeNode = _
+    var right: TreeNode = _
+  }
+
+  def longestUnivaluePath(root: TreeNode): Int = {
+
+    def _recursive(node: TreeNode, value: Int): Int = {
+      if (node == null || node.value != value) {
+        0
+      } else {
+        1 + _recursive(node.left, value) max _recursive(node.right, value)
+      }
+    }
+
+    if (root == null) {
+      0
+    } else {
+      val sub = longestUnivaluePath(root.left) max longestUnivaluePath(root.right)
+      sub + _recursive(root.left, root.value) max _recursive(root.right, root.value)
+    }
+
+  }
+
+  def main(args: Array[String]): Unit = {
+
+  }
 
 }
