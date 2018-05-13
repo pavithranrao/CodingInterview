f4c244ea128a87517a9bf75379a20cbc316b307b
Pavithran <pavithranrao.1512@gmail.com>
2018-05-13 01:19:30 -0400

    Average of Levels in BT LeetCode

---
+++ src/AverageOfLevels.scala
@@ -0,0 +1,51 @@
+import scala.annotation.tailrec
+
+object AverageOfLevels {
+
+  def main(args: Array[String]): Unit = {
+    val root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(3)
+
+    val answer = averageOfLevels(root)
+    println(answer.mkString(", "))
+  }
+  
+  def averageOfLevels(root: TreeNode): Array[Double] = {
+    if (root != null) {
+      bfs(root).map {
+        level =>
+          Util.average(level.map(_.value))
+      }.toArray
+    } else {
+      Array[Double]()
+    }
+  }
+
+  def bfs(start: TreeNode): List[List[TreeNode]] = {
+    @tailrec
+    def bfs_helper(vertices: List[TreeNode],
+                   visited: List[List[TreeNode]],
+                   visitedSet: Set[TreeNode] = Set[TreeNode]()): List[List[TreeNode]] = {
+      val fringe = vertices
+        .flatMap { vertex => List(vertex.left, vertex.right) }
+        .filterNot { child => child == null || visitedSet.contains(child) }
+
+      if (fringe.isEmpty) {
+        visited
+      } else {
+        bfs_helper(fringe, visited :+ fringe, visitedSet ++ fringe)
+      }
+    }
+
+    bfs_helper(List(start), List(List(start)))
+  }
+
+  case class TreeNode(var _value: Int) {
+    var value: Int = _value
+    var left: TreeNode = _
+    var right: TreeNode = _
+  }
+
+
+}
