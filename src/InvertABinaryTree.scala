1ade4eb6165a9ae2dc0f8813cd6ed59a2a443b07
Pavithran <pavithranrao.1512@gmail.com>
2018-05-16 00:09:26 -0400

    Invert a Binary Tree
    Max Howell Google Question

---
+++ src/InvertABinaryTree.scala
@@ -0,0 +1,55 @@
+object InvertABinaryTree {
+
+  def main(args: Array[String]): Unit = {
+    val root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(3)
+
+    invertTree(root)
+  }
+
+  def invertTree(node: TreeNode): TreeNode = {
+    if (node == null) {
+      // Base Case:
+      //   cannot invert a node which is null
+      //   return the same node
+      node
+    } else {
+      // invert subtrees
+      val invertedLeft = invertTree(node.left)
+      val invertedRight = invertTree(node.right)
+
+      // assign inverted subtrees
+      node.left = invertedRight
+      node.right = invertedLeft
+
+      // return the root with inverted subtrees
+      node
+    }
+  }
+
+  def invertTree2(node: TreeNode): TreeNode = {
+    if (node == null) {
+      // Base Case:
+      //   cannot invert a node which is null
+      //   return the same node
+      node
+    } else {
+      val temp = node.left
+
+      node.left = node.right
+      node.right = temp
+      invertTree(node.left)
+      invertTree(node.right)
+
+      // return the root with inverted subtrees
+      node
+    }
+  }
+
+  case class TreeNode(value: Int) {
+    var left: TreeNode = _
+    var right: TreeNode = _
+  }
+
+}
