0b3fd0e74ca2757a8e45de0dc5527241b0f08877
Pavithran <pavithranrao.1512@gmail.com>
2018-05-24 00:00:35 -0400

    Student Attendance Record I

---
+++ src/AttendanceCheck.scala
@@ -0,0 +1,52 @@
+import scala.annotation.tailrec
+
+
+// https://leetcode.com/problems/student-attendance-record-i/description/
+object AttendanceCheck {
+
+  def main(args: Array[String]): Unit = {
+    val inputs = Array("PPALLP", "PPALLL", "LLL")
+    val outputs = inputs.map(checkRecord)
+
+    assert(outputs sameElements Array(true, false, false))
+  }
+
+  // beats 100%
+  def checkRecord(s: String): Boolean = {
+    val len = s.length
+
+    @tailrec
+    def check(idx: Int = 0,
+              absentBefore: Boolean = false): Boolean = {
+      // you survived!
+      if (idx == len) {
+        true
+      } else {
+        s(idx) match {
+          case 'P' =>
+            // present, move to next day
+            check(idx + 1, absentBefore)
+          case 'A' =>
+            // if absent before, false
+            if (absentBefore) {
+              false
+            } else {
+              // else strike 1
+              check(idx + 1, absentBefore = true)
+            }
+          case _ =>
+            // if last consecutively for 3 days, false
+            if (idx >= 2 && s(idx - 1) == 'L' && s(idx - 2) == 'L') {
+              false
+            } else {
+              // don't be late tomorrow
+              check(idx + 1, absentBefore)
+            }
+        }
+      }
+    }
+
+    check()
+  }
+
+}
