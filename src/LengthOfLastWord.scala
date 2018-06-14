2cfebf35de6009b6d9d44e79124fd16de554f935
Pavithran <pavithranrao.1512@gmail.com>
2018-06-13 21:47:18 -0400

    Length of Last Word LeetCode

---
+++ src/LengthOfLastWord.scala
@@ -0,0 +1,39 @@
+import scala.annotation.tailrec
+
+object LengthOfLastWord {
+
+  def main(args: Array[String]): Unit = {
+    val s = "abc def"
+    println(lengthOfLastWord(s) == 3)
+    println(lengthOfLastWord(" ") == 0)
+    println(lengthOfLastWord("a") == 1)
+    println(lengthOfLastWord("a ") == 1)
+  }
+
+  // beats 87%
+  def lengthOfLastWord(s: String): Int = {
+    val len = s.length
+
+    @tailrec
+    def helper(idx: Int = len - 1,
+               lengthOfLast: Int = 0)
+              (implicit sawChar: Boolean = false): Int = {
+      if (idx == -1) {
+        lengthOfLast
+      } else {
+        (s(idx), sawChar) match {
+          case (' ', true) => lengthOfLast
+          case (' ', false) => helper(idx - 1)(sawChar)
+          case (_, _) => helper(idx - 1, lengthOfLast + 1)(sawChar = true)
+        }
+      }
+    }
+
+    if (len > 0)
+      helper()
+    else
+      0
+  }
+
+
+}
