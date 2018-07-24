ea75cca557a96f7ad41d57336d2b1cab11f6ebd0
Pavithran <pavithranrao.1512@gmail.com>
2018-07-23 20:04:16 -0400

    Search BST LeetCode

---
+++ src/SearchBST.scala
@@ -0,0 +1,38 @@
+import scala.annotation.tailrec
+
+object SearchBST {
+
+  // beats 100%
+  @tailrec
+  def searchBST(root: TreeNode)
+               (implicit target: Int): TreeNode = {
+    if (root == null) {
+      null
+    } else {
+      root.value.compare(target) match {
+        case 0 => root
+        case 1 => searchBST(root.left)
+        case -1 => searchBST(root.right)
+      }
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val root = new TreeNode(2)
+    val left = new TreeNode(1)
+    val right = new TreeNode(3)
+
+    root.left = left
+    root.right = right
+
+    val answer = searchBST(root)(3)
+    assert(answer == right)
+  }
+
+  class TreeNode(var _value: Int) {
+    var value: Int = _value
+    var left: TreeNode = _
+    var right: TreeNode = _
+  }
+
+}
