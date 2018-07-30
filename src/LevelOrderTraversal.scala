07f485cd2a1a32c1461a7f683b8d46e010adde22
Pavithran <pavithranrao.1512@gmail.com>
2018-07-29 21:15:41 -0400

    minor correction

--- src/LevelOrderTraversal.scala
+++ src/LevelOrderTraversal.scala
@@ -24,16 +24,15 @@ object LevelOrderTraversal {
   def bfs(start: TreeNode): List[List[TreeNode]] = {
     @tailrec
     def bfs_helper(vertices: List[TreeNode],
-                   visited: List[List[TreeNode]],
-                   visitedSet: Set[TreeNode] = Set[TreeNode]()): List[List[TreeNode]] = {
+                   visited: List[List[TreeNode]]): List[List[TreeNode]] = {
       val fringe = vertices
-        .flatMap { vertex => List(vertex.left, vertex.right) }.distinct
-        .filterNot { child => child == null || visitedSet.contains(child) }
+        .flatMap { vertex => List(vertex.left, vertex.right) }
+        .filterNot(_ == null)
 
       if (fringe.isEmpty) {
         visited
       } else {
-        bfs_helper(fringe, visited :+ fringe, visitedSet ++ fringe)
+        bfs_helper(fringe, visited :+ fringe)
       }
     }
 
